96a5170e1c830fa987f847096ff53155d6a6dd9a
Gapi505 <81484469+Gapi505@users.noreply.github.com>
2024-05-11 12:51:21 +0200

    Refactor main.py to include async functionality for transcription and conversation loop. Update prompt_template.yaml to use the 'message' field instead of 'user_message'.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from llama_cpp import Llama
-import gc
-import torch
-import yaml
-from transformers import pipeline
 from transformers.pipelines.audio_utils import ffmpeg_microphone_live
-import sys
+from pyannote.audio.pipelines import VoiceActivityDetection
+from transformers import pipeline
+from pyannote.audio import Model
+from llama_cpp import Llama
+import numpy as np
 import asyncio
+import dotenv
+import torch
+import time
+import yaml
+import sys
+import gc
+import os
+
+dotenv.load_dotenv()
 
 
 prompt_template = None
@@ -15,22 +23,20 @@ with open("templates/prompt_template.yaml", "r") as f:
 with open("templates/system_prompt.yaml", "r") as f:
     system_prompt_unconstructed = yaml.safe_load(f)
 
-uncertan_conversation = ""
-certan_conversation = ""
-
-uncertan_message = ""
-certan_message = ""
+conversation = ""
 
 
 llm = None
 transcriber = None
+vad = None
 
 
 def free_memory():
-    global llm, transcriber
+    global llm, transcriber, vad
     
     llm = None
     transcriber = None
+    vad = None
     gc.collect()
     torch.cuda.empty_cache()
 
@@ -50,8 +56,23 @@ def load_llm():
 def load_transcriber():
     global transcriber
     transcriber = pipeline(
-        "automatic-speech-recognition", model="openai/whisper-base.en", device="cuda"
+        "automatic-speech-recognition", model="openai/whisper-tiny.en", device="cuda"
     )
+
+def load_vad():
+    global vad
+    auth_token = os.getenv("SEGMENTATION_SEGMENTATION_TOKEN")
+    vad_model = Model.from_pretrained(
+                "pyannote/segmentation-3.0", 
+                use_auth_token=auth_token)
+    vad = VoiceActivityDetection(segmentation=vad_model)
+    HYPER_PARAMETERS = {
+    # remove speech regions shorter than that many seconds.
+    "min_duration_on": 0.0,
+    # fill non-speech regions shorter than that many seconds.
+    "min_duration_off": 0.25,
+    }
+    vad.instantiate(HYPER_PARAMETERS)
 
 def construct_prompt(conversation):
     with open("templates/system_prompt.yaml", "r") as f:
@@ -62,6 +83,9 @@ def construct_prompt(conversation):
     for conf in config:
         system_prompt += system_prompt_unconstructed[conf] + "\n"
     
+    conversation = conversation.rstrip("\n")
+    system_prompt = system_prompt.rstrip("\n")
+    
     prompt = prompt_template["prompt"].format(system_prompt=system_prompt,conversation=conversation)
 
     return prompt
@@ -71,8 +95,7 @@ def construct_message(user, message):
     return fmessage
 
 
-async def transcribe(chunk_length_s=100.0, stream_chunk_s=0.5):
-    global uncertan_message
+async def transcribe(chunk_length_s=100.0, stream_chunk_s=0.25, stop_pause_time=0.0):
     sampling_rate = transcriber.feature_extractor.sampling_rate
 
     mic = ffmpeg_microphone_live(
@@ -82,40 +105,70 @@ async def transcribe(chunk_length_s=100.0, stream_chunk_s=0.5):
     )
 
     print("Start speaking...")
-    previousTranscription = None
-    repetitionCount = 0
+    speaking_start = None
     for transcription in transcriber(mic, generate_kwargs={"max_new_tokens": 128}):
-        sys.stdout.write("\033[K")
-        print(transcription["text"], end="\r")
-        uncertan_message = construct_message("user", transcription["text"])
-        if previousTranscription and transcription["text"] == previousTranscription["text"]:
-            repetitionCount += 1
-        else:
-            repetitionCount = 0
         
-        if repetitionCount > 3:
-            break
+        sys.stdout.write("\033[K")
+        print(transcription["text"], end="\n")
+        
+        # End tests
+        chunk = next(mic)
+        audio_data = chunk['raw']
+        waveform = audio_data / np.max(np.abs(audio_data))
+        waveform_tensor = torch.from_numpy(waveform).float().reshape(1, -1)
+        segments = vad({"waveform": waveform_tensor, "sample_rate": chunk['sampling_rate']}).get_timeline().support()
+        print(segments)
+        if speaking_start is None:
+            speaking_start = time.time()
+        if len(segments) > 0:
+            elapsed_time = time.time() - speaking_start
+            print(elapsed_time)
+            last_segment = segments[-1]
+            end_time = last_segment.end
+            if elapsed_time - end_time > stop_pause_time:
+                break
+
+
+
         if not transcription["partial"][0]:
             break
-        previousTranscription = transcription
 
     return transcription["text"]
 
+async def llm_response(prompt):
+    kwargs = {
+        "max_tokens": 128,
+        "temperature": 0.8,
+    }
+    if llm is None:
+        load_llm()
+    print(prompt)
+    response = llm(prompt, **kwargs,)
+    print(response)
+    return response
+
 load_transcriber()
 load_llm()
+load_vad()
+
+print(torch.cuda.memory_summary(device="cuda", abbreviated=False) )
 
 async def main_loop():
-    global uncertan_conversation, certan_conversation, uncertan_message, certan_message
+    global conversation
     transcriptionJob = asyncio.create_task(transcribe())
     print("Start conversation")
     while True:
-        if transcriptionJob.done():
-            transcription = transcriptionJob.result()
-            print(transcription)
-            break
+        transcription = await transcriptionJob
+        print(transcription)
+        message = construct_message("user", transcription)
+        conversation += message + "\n"
+        prompt = construct_prompt(conversation)
+        print(prompt)
+        response = await llm_response(prompt)
+        response = response["choices"][0]["text"]
+        print(response)
+        conversation += construct_message("assistant", response) + "\n"
+        transcriptionJob = asyncio.create_task(transcribe())
         #print(uncertan_message)
-        uncertan_conversation = certan_conversation + "\n" + uncertan_message
-        uncertan_prompt = construct_prompt(uncertan_conversation)
-        await asyncio.sleep(1)
 
 asyncio.run(main_loop())
